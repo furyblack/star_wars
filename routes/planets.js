fec355bd22ea721c9d96fa3141dc14ab30fa5b3d
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-04-27 21:01:15 +0400

    fix planet name on the planet page

--- routes/planets.js
+++ routes/planets.js
@@ -16,28 +16,30 @@ router.get('/:id', async (req, res, next) => {
     try {
         const response = await axios.get(`https://sw-api.starnavi.io/people/?homeworld=${req.params.id}`);
         const people = response.data.results;
-        res.render('planet', { people, title: `People from ${req.params.id}`  });
+
+
+        const planetResponse = await axios.get(`https://sw-api.starnavi.io/planets/${req.params.id}`);
+        const planet = planetResponse.data;
+        res.render('planet', { people,  title: planet.name, planet: planet  });
 
     } catch (error) {
-        next(error); // передаём ошибку дальше, Express сам её обработает
+        next(error);
     }
 });
 
 router.get('/people/:name', async (req, res, next) => {
     try {
-        // Отправляем запрос, используя имя персонажа
         const response = await axios.get(`https://sw-api.starnavi.io/people/?name=${req.params.name}`);
-        const pers = response.data.results;  // Извлекаем результаты
+        const pers = response.data.results;
 
-        // Маппинг имен персонажей к изображениям
+
+        //маппинг имен персонажей к изображениям
         const characterImages = {
             "Luke Skywalker": "luke-skywalker.jpg",
             "Darth Vader": "darth-vader.jpg",
             "Han Solo": "han-solo.jpg",
-            // добавь остальные персонажи
         };
-
-        // Добавляем картинку в данные персонажа
+        //добавляем картинку в данные персонажа
         const personWithImage = pers.map(person => {
             return {
                 ...person,
